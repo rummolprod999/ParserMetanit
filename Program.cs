f6435a776fb320ea69660f324c653d63627063ca
rummolprod999 <rummolprod999@gmail.com>
2017-12-26 11:53:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParserMetanit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
